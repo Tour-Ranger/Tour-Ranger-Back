6691730aff56b5d98927047397f5f8229ffebac6
JisooPyo <audwl326@gmail.com>
2023-08-18 01:58:36 +0900

    init commit

---
+++ src/main/java/com/tourranger/TourRangerApplication.java
@@ -0,0 +1,13 @@
+package com.tourranger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourRangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourRangerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourranger/TourRangerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourranger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourRangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
