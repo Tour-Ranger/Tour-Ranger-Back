403fd1fbcc70c721c015759008b4d759420843ad
JisooPyo <audwl326@gmail.com>
2023-08-19 17:36:03 +0900

    complete: Item Dummy data 삽입 코드 테스트 완료

--- src/test/java/com/tourranger/common/DummyDataTest.java
+++ src/test/java/com/tourranger/common/DummyDataTest.java
@@ -1,6 +1,8 @@
 package com.tourranger.common;
 
 import com.tourranger.common.dummy.DummyGenerator;
+import com.tourranger.item.entity.Item;
+import com.tourranger.item.repository.ItemRepository;
 import com.tourranger.user.entity.User;
 import com.tourranger.user.repository.UserRepository;
 import org.junit.jupiter.api.*;
@@ -18,14 +20,23 @@ public class DummyDataTest {
 	private DummyGenerator dummyGenerator;
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private ItemRepository itemRepository;
 
 	@Test
 	@DisplayName("Dummy Generator Test")
 	void dummyGeneratorTest() {
+		// dummyUserGeneretor Test
 		List<User> userList1 = userRepository.findAll();
 		dummyGenerator.dummyUserGenerator();
 		List<User> userList2 = userRepository.findAll();
 
+		// dummyItemGenerator Test
+		List<Item> itemList1 = itemRepository.findAll();
+		dummyGenerator.dummyItemGenerator();
+		List<Item> itemList2 = itemRepository.findAll();
+
 		assertEquals(DummyGenerator.COUNT, userList2.size() - userList1.size());
+		assertEquals(1, itemList2.size() - itemList1.size());
 	}
 }
