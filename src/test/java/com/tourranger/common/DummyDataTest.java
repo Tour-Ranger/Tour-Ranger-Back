78d42e3aa2f48a00461d6ba54a45be5dfa44cdf9
JisooPyo <130378232+JisooPyo@users.noreply.github.com>
2023-08-21 09:26:27 +0900

    [feature] Item 더미 데이터 삽입 코드 구현 (#7)
    * update: package 이름 order -> purchase 변경
    
    * complete: ItemRepository 구현
    
    * complete: Item Dummy data 삽입 코드 구현 완료
    
    * complete: Item Dummy data 삽입 코드 테스트 완료
    
    * update: 더미데이터 코드 주석처리
    
    * update: 아이템 더미데이터 테스트 코드 변경
    
    정해준 값으로 잘 생성되었는지 체크하게끔 변경

--- src/test/java/com/tourranger/common/DummyDataTest.java
+++ src/test/java/com/tourranger/common/DummyDataTest.java
@@ -1,6 +1,8 @@
 package com.tourranger.common;
 
 import com.tourranger.common.dummy.DummyGenerator;
+import com.tourranger.item.entity.Item;
+import com.tourranger.item.repository.ItemRepository;
 import com.tourranger.user.entity.User;
 import com.tourranger.user.repository.UserRepository;
 import org.junit.jupiter.api.*;
@@ -8,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -18,14 +21,34 @@ public class DummyDataTest {
 	private DummyGenerator dummyGenerator;
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private ItemRepository itemRepository;
 
 	@Test
 	@DisplayName("Dummy Generator Test")
 	void dummyGeneratorTest() {
+		// dummyUserGeneretor Test
 		List<User> userList1 = userRepository.findAll();
 		dummyGenerator.dummyUserGenerator();
 		List<User> userList2 = userRepository.findAll();
 
+		// dummyItemGenerator Test
+		dummyGenerator.dummyItemGenerator();
+		Optional<Item> item = itemRepository.findById(1L);
+		String name = "5박 6일 유럽 여행 초특가 할인!";
+		Long price = 3_000_000L; // 3백만원
+		Long discountPrice = 100L; // 백원
+		Long quantity = 30L; // 30개
+		Long period = 5L; // 5박 6일
+
+		// User는 정해진 개수만큼(COUNT) 잘 생성되었는가?
 		assertEquals(DummyGenerator.COUNT, userList2.size() - userList1.size());
+
+		// item은 정해준 값으로 잘 생성되었는가?
+		assertEquals(name, item.get().getName());
+		assertEquals(price, item.get().getPrice());
+		assertEquals(discountPrice, item.get().getDiscountPrice());
+		assertEquals(quantity, item.get().getQuantity());
+		assertEquals(period, item.get().getPeriod());
 	}
 }
