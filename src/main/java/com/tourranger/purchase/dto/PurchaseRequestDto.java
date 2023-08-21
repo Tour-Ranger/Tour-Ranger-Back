72429cf1f8265d81629b622f2e7140d2e4ac907d
JisooPyo <audwl326@gmail.com>
2023-08-21 15:40:53 +0900

    update: PurchaseRequestDto 어노테이션 추가
    @NotBlank, @Email 추가하여 유효성 검사

--- src/main/java/com/tourranger/purchase/dto/PurchaseRequestDto.java
+++ src/main/java/com/tourranger/purchase/dto/PurchaseRequestDto.java
@@ -1,10 +1,14 @@
 package com.tourranger.purchase.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
 public class PurchaseRequestDto {
+	@NotBlank
+	@Email
 	private String email;
 }
