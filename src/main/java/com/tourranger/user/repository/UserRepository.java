b5a79d165040ec1e415defe879ee94a317ced15f
JisooPyo <audwl326@gmail.com>
2023-08-21 15:41:39 +0900

    update: email로 User찾는 메서드 추가

--- src/main/java/com/tourranger/user/repository/UserRepository.java
+++ src/main/java/com/tourranger/user/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.tourranger.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
+	Optional<User> findByEmail(String email);
 }
