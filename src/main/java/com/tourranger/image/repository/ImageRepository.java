eaaabfb66f3d93822924fbfe87570946cdc268cc
Jo Woojin <dnwls3525@gmail.com>
2023-08-21 11:26:39 +0900

    refactor: 정의할 필요없는 findById 메서드 삭제

--- src/main/java/com/tourranger/image/repository/ImageRepository.java
+++ src/main/java/com/tourranger/image/repository/ImageRepository.java
@@ -9,5 +9,4 @@ import com.tourranger.image.entity.Image;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image,Long>, ImageRepositoryCustom {
-	Optional<Image> findById(Long Id);
 }
