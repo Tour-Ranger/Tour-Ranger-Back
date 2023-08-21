4fb016dc8bd67c38bb8239dddb3932449b51c290
JisooPyo <audwl326@gmail.com>
2023-08-21 16:25:19 +0900

    update: UserRepository -> UserServiceImpl 가져오기

--- src/main/java/com/tourranger/purchase/service/PurchaseServiceImpl.java
+++ src/main/java/com/tourranger/purchase/service/PurchaseServiceImpl.java
@@ -8,7 +8,7 @@ import com.tourranger.purchase.dto.PurchaseRequestDto;
 import com.tourranger.purchase.entity.Purchase;
 import com.tourranger.purchase.repository.PurchaseRepository;
 import com.tourranger.user.entity.User;
-import com.tourranger.user.repository.UserRepository;
+import com.tourranger.user.service.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,25 +16,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class PurchaseServiceImpl implements PurchaseService {
-	private final UserRepository userRepository;
+	private final UserServiceImpl userService;
 	private final ItemRepository itemRepository;
 	private final PurchaseRepository purchaseRepository;
 
 	@Override
 	@Transactional
 	public void purchaseItem(Long itemId, PurchaseRequestDto requestDto) {
-		User user = findUser(requestDto.getEmail());
+		User user = userService.findUser(requestDto.getEmail());
 		Item item = findItem(itemId);
 		checkStock(item);
 		Purchase purchase = Purchase.builder().item(item).user(user).build();
 		purchaseRepository.save(purchase);
 		item.sellOne();
-	}
-
-	private User findUser(String email) {
-		return userRepository.findByEmail(email).orElseThrow(() ->
-				new CustomException(CustomErrorCode.USER_NOT_FOUND, null)
-		);
 	}
 
 	private Item findItem(Long id) {
