bfbc151f8110af431e16dcd6dae252b8f6f60e4a
JisooPyo <audwl326@gmail.com>
2023-08-23 00:44:01 +0900

    complete: getItem() API 구현 완료

---
+++ src/main/java/com/tourranger/item/dto/ItemResponseDto.java
@@ -0,0 +1,27 @@
+package com.tourranger.item.dto;
+
+import com.tourranger.item.entity.Item;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ItemResponseDto {
+	private Long id;
+	private String name;
+	private Long price;
+	private Long discountPrice;
+	private Long quantity;
+	private Long period;
+
+	public ItemResponseDto(Item item) {
+		this.id = item.getId();
+		this.name = item.getName();
+		this.price = item.getPrice();
+		this.discountPrice = item.getDiscountPrice();
+		this.quantity = item.getQuantity();
+		this.period = item.getPeriod();
+	}
+}
--- src/main/java/com/tourranger/item/service/ItemServiceImpl.java
+++ src/main/java/com/tourranger/item/service/ItemServiceImpl.java
@@ -1,24 +1,29 @@
 package com.tourranger.item.service;
 
-import org.springframework.stereotype.Service;
-
 import com.tourranger.common.error.CustomErrorCode;
 import com.tourranger.common.exception.CustomException;
-import com.tourranger.image.service.ImageService;
+import com.tourranger.item.dto.ItemResponseDto;
 import com.tourranger.item.entity.Item;
 import com.tourranger.item.repository.ItemRepository;
-import com.tourranger.item.repository.ItemRepositoryCustom;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
-	ItemRepository itemRepository;
+
+	private final ItemRepository itemRepository;
+	@Override
+	public ItemResponseDto getItem(Long itemId) {
+		Item item = findItem(itemId);
+		ItemResponseDto responseDto = new ItemResponseDto(item);
+		return responseDto;
+	}
 
 	public Item findItem(Long id) {
 		return itemRepository.findById(id).orElseThrow(() ->
-			new CustomException(CustomErrorCode.ITEM_NOT_FOUND, null)
+				new CustomException(CustomErrorCode.ITEM_NOT_FOUND, null)
 		);
 	}
+
 }
