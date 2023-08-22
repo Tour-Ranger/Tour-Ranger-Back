81b85e78f36cec25e145a76d5077cd87952e2b07
Jo Woojin <dnwls3525@gmail.com>
2023-08-22 15:51:51 +0900

    update: 복수 개의 파일 저장 메서드 추가

--- src/main/java/com/tourranger/common/file/FileStore.java
+++ src/main/java/com/tourranger/common/file/FileStore.java
@@ -2,6 +2,8 @@ package com.tourranger.common.file;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -21,7 +23,6 @@ public class FileStore {
 			//이미지가 아닌 경우, Exception 처리
 			throw new IllegalArgumentException("이미지 파일만 업로드할 수 있습니다.");
 		}
-
 		//IE나 Edge는 전체 경로가 들어오므로, 실제 파일 이름을 불러오는 메서드 사용
 		String originalFileName = multipartFile.getOriginalFilename();
 		//동일 파일명을 피하기 위해 랜덤값 사용하여 저장
@@ -29,6 +30,16 @@ public class FileStore {
 		//서버에 파일 저장
 		multipartFile.transferTo(new File(getFullPath(saveFileName)));
 		return saveFileName;
+	}
+
+	public List<String> saveFiles(List<MultipartFile> multipartFileList) throws IOException{
+		List<String> saveFileList = new ArrayList<>();
+		for (MultipartFile multipartFile : multipartFileList){
+			if(!multipartFile.isEmpty()){
+				saveFileList.add(saveFile(multipartFile));
+			}
+		}
+		return saveFileList;
 	}
 
 	//파일 삭제 메서드
@@ -41,7 +52,7 @@ public class FileStore {
 	}
 
 	// 파일 저장 이름 만들기
-	// - 사용자들이 올리는 파일 이름이 같을 수 있으므로, 자체적으로 랜덤 이름을 만들어 사용한다
+	// - 사용자들이 올리는 파일 이름이 같을 수 있으므로, 중복 방지를 위해 자체적으로 랜덤 이름을 만들어 사용한다
 	private String createSaveFileName(String originalFilename) {
 		String ext = extractExt(originalFilename);
 		String uuid = UUID.randomUUID().toString();
