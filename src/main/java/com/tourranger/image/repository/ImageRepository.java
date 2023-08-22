3510999efa564eb624f65ddcbffde1b9230ec5b0
Jo Woojin <dnwls3525@gmail.com>
2023-08-22 22:20:39 +0900

    update: Item에 속한 이미지전체를 조회하는 메서드 추가

--- src/main/java/com/tourranger/image/repository/ImageRepository.java
+++ src/main/java/com/tourranger/image/repository/ImageRepository.java
@@ -1,12 +1,15 @@
 package com.tourranger.image.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tourranger.image.entity.Image;
+import com.tourranger.item.entity.Item;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image,Long>, ImageRepositoryCustom {
+	List<Image> findAllByItem(Item item);
 }
