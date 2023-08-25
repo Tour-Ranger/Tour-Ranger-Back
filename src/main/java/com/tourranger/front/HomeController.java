aea3da3d19cb1edaf849f89c16c938df20c83a20
yesrin <asjs11@naver.com>
2023-08-25 11:30:31 +0900

    update: item 조회, image 조회 api 연결

--- src/main/java/com/tourranger/front/HomeController.java
+++ src/main/java/com/tourranger/front/HomeController.java
@@ -3,23 +3,19 @@ package com.tourranger.front;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.tourranger.item.service.ItemService;
-
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
-	private final ItemService itemService;
 
 	@GetMapping("/")
 	public String bannerPage() {
 		return "bannerPage";
 	}
 
-	@GetMapping("/items")
+	@GetMapping("/items/1")
 	public String tourItem() {
 		return "tourItemPage";
 	}
-
 }
