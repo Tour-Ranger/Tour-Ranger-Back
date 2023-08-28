bac3c15df4a8e0410697996dacd12fb4b607d756
yesrin <asjs11@naver.com>
2023-08-28 23:44:10 +0900

    fix: redis 조회 오류 수정

--- src/main/java/com/tourranger/item/service/ItemServiceImpl.java
+++ src/main/java/com/tourranger/item/service/ItemServiceImpl.java
@@ -1,19 +1,24 @@
 package com.tourranger.item.service;
 
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
 import com.tourranger.common.error.CustomErrorCode;
 import com.tourranger.common.exception.CustomException;
 import com.tourranger.item.dto.ItemResponseDto;
 import com.tourranger.item.entity.Item;
 import com.tourranger.item.repository.ItemRepository;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
 
 	private final ItemRepository itemRepository;
+
 	@Override
+	@Cacheable(key = "#itemId", value = "ITEM")
 	public ItemResponseDto getItem(Long itemId) {
 		Item item = findItem(itemId);
 		ItemResponseDto responseDto = new ItemResponseDto(item);
@@ -22,7 +27,7 @@ public class ItemServiceImpl implements ItemService {
 
 	public Item findItem(Long id) {
 		return itemRepository.findById(id).orElseThrow(() ->
-				new CustomException(CustomErrorCode.ITEM_NOT_FOUND, null)
+			new CustomException(CustomErrorCode.ITEM_NOT_FOUND, null)
 		);
 	}
 
