cf170239737d088b71210c3fd827ce6aab86280d
Jo Woojin <dnwls3525@gmail.com>
2023-08-28 23:01:06 +0900

    update : 3종 이미지 엔티티 추가에 따른 Item 속성 추가

--- src/main/java/com/tourranger/item/entity/Item.java
+++ src/main/java/com/tourranger/item/entity/Item.java
@@ -1,6 +1,9 @@
 package com.tourranger.item.entity;
 
+import com.tourranger.image.entity.Airline;
 import com.tourranger.image.entity.Image;
+import com.tourranger.image.entity.ThumbnailImage;
+import com.tourranger.image.entity.TravelAgency;
 import com.tourranger.purchase.entity.Purchase;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -35,6 +38,19 @@ public class Item {
 
 	@Column(nullable = false)
 	private Long period;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "travel_agency_id", nullable = false)
+	private TravelAgency travelAgency;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "thumbnail_image_id", nullable = false)
+	private ThumbnailImage thumbnailImage;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "airline_id", nullable = false)
+	private Airline airline;
+
 
 	@Builder.Default
 	@OneToMany(mappedBy = "item", orphanRemoval = true)
