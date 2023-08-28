0ea5e8659f24dac80ec5c6d23bab1c5c9ac521b7
Jo Woojin <dnwls3525@gmail.com>
2023-08-28 22:58:54 +0900

    update : Airline, ThumbnailImage, TravelAgency 엔티티추가

---
+++ src/main/java/com/tourranger/image/entity/Airline.java
@@ -0,0 +1,35 @@
+package com.tourranger.image.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.tourranger.item.entity.Item;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Airline {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String url;
+
+	@Builder.Default
+	@OneToMany(mappedBy = "airline", orphanRemoval = true)
+	private List<Item> itemList = new ArrayList<>();
+}
