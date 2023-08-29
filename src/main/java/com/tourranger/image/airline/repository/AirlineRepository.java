fc6f208d7aeadcdacfe43c2277ba1732e1dea84f
Jo Woojin <dnwls3525@gmail.com>
2023-08-29 14:10:38 +0900

    update : airlineRepository 애노테이션 추가

--- src/main/java/com/tourranger/image/airline/repository/AirlineRepository.java
+++ src/main/java/com/tourranger/image/airline/repository/AirlineRepository.java
@@ -1,8 +1,10 @@
 package com.tourranger.image.airline.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tourranger.image.airline.entity.Airline;
 
+@Repository
 public interface AirlineRepository extends JpaRepository<Airline, Long>, AirlineRepositoryCustom {
 }
