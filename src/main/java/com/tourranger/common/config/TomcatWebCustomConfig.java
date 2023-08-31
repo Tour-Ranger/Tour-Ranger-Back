6b31d16ed6c189a07e407a8ca8b12aad2091da72
jungeun-choi <jungeun5.choi@gmail.com>
2023-09-01 00:40:07 +0900

    Init: TomcatWebCustomConfig - API URI 인코딩

---
+++ src/main/java/com/tourranger/common/config/TomcatWebCustomConfig.java
@@ -0,0 +1,13 @@
+package com.tourranger.common.config;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatWebCustomConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+	@Override
+	public void customize(TomcatServletWebServerFactory factory) {
+		factory.addConnectorCustomizers(connector -> connector.setProperty("relaxedQueryChars", "<>[\\]^`{|}"));
+	}
+}
