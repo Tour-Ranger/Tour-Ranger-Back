f9c06424032c4aa456c463ffa94d39862dbd2de7
VVooJIN3 <131952511+VVooJIN3@users.noreply.github.com>
2023-09-01 20:56:01 +0900

    [feature] 배너페이지 화면 기능추가 (#39)
    * update: 이름조회 시 조건별 메서드추가
    
    * feature: 일반 조회시, 검색어 조회 시 ItemList를 가져오는 API추가
    
    * feature: ItemList조회, 자세히보기 버튼을 통한 상세페이지 이동, 검색, 페이지네이션
    
    * delete : 미사용필드 제거
    
    * delete : sout제거
    
    * delete : 미사용 Exception제거

--- src/main/java/com/tourranger/item/repository/ItemRepository.java
+++ src/main/java/com/tourranger/item/repository/ItemRepository.java
@@ -1,6 +1,8 @@
 package com.tourranger.item.repository;
 
 import com.tourranger.item.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,15 @@ import java.util.Optional;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long>, ItemRepositoryCustom {
 	Optional<Item> findTopByOrderByIdDesc();
+
+	Page<Item> findAllByOrderById(Pageable pageable);
+
+	//이름검색(조건-최신순)
+	Page<Item> findByNameContainingOrderByIdDesc(String search, Pageable pageable);
+
+	//이름검색(조건-가격 낮은순)
+	Page<Item> findByNameContainingOrderByDiscountPrice(String search, Pageable pageable);
+
+	//이름검색(조건-가격 높은순)
+	Page<Item> findByNameContainingOrderByDiscountPriceDesc(String search, Pageable pageable);
 }
--- src/main/java/com/tourranger/item/service/ItemServiceImpl.java
+++ src/main/java/com/tourranger/item/service/ItemServiceImpl.java
@@ -6,24 +6,47 @@ import com.tourranger.item.dto.ItemResponseDto;
 import com.tourranger.item.entity.Item;
 import com.tourranger.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
 
-	private final ItemRepository itemRepository;
-	@Override
-	public ItemResponseDto getItem(Long itemId) {
-		Item item = findItem(itemId);
-		ItemResponseDto responseDto = new ItemResponseDto(item);
-		return responseDto;
-	}
+    private final ItemRepository itemRepository;
 
-	public Item findItem(Long id) {
-		return itemRepository.findById(id).orElseThrow(() ->
-				new CustomException(CustomErrorCode.ITEM_NOT_FOUND, null)
-		);
-	}
+    @Override
+    public ItemResponseDto getItem(Long itemId) {
+        Item item = findItem(itemId);
+        ItemResponseDto responseDto = new ItemResponseDto(item);
+        return responseDto;
+    }
+
+    @Override
+    public List<ItemResponseDto> getItemList(Pageable pageable) {
+        return itemRepository.findAllByOrderById(pageable).stream().map(ItemResponseDto::new).toList();
+    }
+
+    @Override
+    public List<ItemResponseDto> getSearchedItemList(String search, String condition, Pageable pageable) {
+
+        return switch (condition) {
+            case "latest" -> // 최신순
+                    itemRepository.findByNameContainingOrderByIdDesc(search, pageable).stream().map(ItemResponseDto::new).toList();
+            case "priceLowToHigh" -> // 가격 낮은순
+                    itemRepository.findByNameContainingOrderByDiscountPrice(search, pageable).stream().map(ItemResponseDto::new).toList();
+            case "priceHighToLow" -> // 가격 높은순
+                    itemRepository.findByNameContainingOrderByDiscountPriceDesc(search, pageable).stream().map(ItemResponseDto::new).toList();
+            default -> null;
+        };
+    }
+
+    public Item findItem(Long id) {
+        return itemRepository.findById(id).orElseThrow(() ->
+                new CustomException(CustomErrorCode.ITEM_NOT_FOUND, null)
+        );
+    }
 
 }
