9b4fe7ce5146fed5a64bf46ea2f7903296be6c1e
woojin <dnwls3525@gmail.com>
2023-09-01 17:21:16 +0900

    feature: 일반 조회시, 검색어 조회 시 ItemList를 가져오는 API추가

--- src/main/java/com/tourranger/item/controller/ItemController.java
+++ src/main/java/com/tourranger/item/controller/ItemController.java
@@ -5,24 +5,39 @@ import com.tourranger.item.service.ItemService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/tour-ranger")
 @Tag(name = "Item API", description = "Item에 관련된 API 정보를 담고 있습니다.")
 public class ItemController {
-	private final ItemService itemService;
+    private final ItemService itemService;
 
-	@Operation(summary = "상품 조회", description = "ItemId에 맞는 상품의 정보를 조회합니다.")
-	@GetMapping("/items/{itemId}")
-	public ResponseEntity<ItemResponseDto> getItem(@PathVariable Long itemId) {
-		ItemResponseDto responseDto = itemService.getItem(itemId);
-		return ResponseEntity.ok().body(responseDto);
-	}
+    @Operation(summary = "상품 조회", description = "ItemId에 맞는 상품의 정보를 조회합니다.")
+    @GetMapping("/items/{itemId}")
+    public ResponseEntity<ItemResponseDto> getItem(@PathVariable Long itemId) {
+        ItemResponseDto responseDto = itemService.getItem(itemId);
+        return ResponseEntity.ok().body(responseDto);
+    }
+
+    @Operation(summary = "상품리스트 조회", description = "상품의 정보를 조회합니다.")
+    @GetMapping("/items")
+    public ResponseEntity<List<ItemResponseDto>> getItemList(@RequestParam(name = "search", required = false) String search,
+                                                          @RequestParam(name = "condition", required = false) String condition,
+                                                          Pageable pageable) throws UnsupportedEncodingException {
+        //검색어 입력으로 조회하는 경우
+        if (search != null && !search.isEmpty()) {
+            return ResponseEntity.ok().body(itemService.getSearchedItemList(search, condition, pageable));
+        }
+        //일반 조회하는 경우-Id순
+        else
+            return ResponseEntity.ok().body(itemService.getItemList(pageable));
+    }
 
 }
