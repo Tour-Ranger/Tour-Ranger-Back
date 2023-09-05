d198489d410e612b372da454c858a5daa073c641
JisooPyo <audwl326@gmail.com>
2023-09-05 11:38:13 +0900

    update: 테스트 코드 위한 메서드 추가

--- src/main/java/com/tourranger/purchase/repository/PurchaseRepository.java
+++ src/main/java/com/tourranger/purchase/repository/PurchaseRepository.java
@@ -4,6 +4,10 @@ import com.tourranger.purchase.entity.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long>, PurchaseRepositoryCustom {
+	// 테스트 코드를 위한 메서드
+	Optional<Purchase> findTopByOrderByIdDesc();
 }
