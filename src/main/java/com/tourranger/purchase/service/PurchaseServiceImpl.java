809c10997416b470f83cdcbfc8d92b6b47964fac
yesrin <asjs11@naver.com>
2023-09-08 23:34:47 +0900

    update: 구매 로직에 Lock 적용

--- src/main/java/com/tourranger/purchase/service/PurchaseServiceImpl.java
+++ src/main/java/com/tourranger/purchase/service/PurchaseServiceImpl.java
@@ -1,10 +1,12 @@
 package com.tourranger.purchase.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.tourranger.common.error.CustomErrorCode;
 import com.tourranger.common.exception.CustomException;
+import com.tourranger.common.namedLock.LockItemRepository;
 import com.tourranger.item.entity.Item;
 import com.tourranger.item.service.ItemServiceImpl;
 import com.tourranger.purchase.dto.PurchaseRequestDto;
@@ -21,16 +23,25 @@ public class PurchaseServiceImpl implements PurchaseService {
 	private final UserServiceImpl userService;
 	private final ItemServiceImpl itemService;
 	private final PurchaseRepository purchaseRepository;
+	private final LockItemRepository lockItemRepository;
 
 	@Override
-	@Transactional
+	@Transactional(propagation = Propagation.REQUIRES_NEW) // 매번 새로운 트랜잭션을 시작
 	public void purchaseItem(Long itemId, PurchaseRequestDto requestDto) {
-		User user = userService.findUser(requestDto.getEmail());
-		Item item = itemService.findItem(itemId);
-		checkStock(item);
-		Purchase purchase = Purchase.builder().item(item).user(user).build();
-		purchaseRepository.save(purchase);
-		item.sellOne();
+		try {
+			//NameLock 획득
+			lockItemRepository.getLock(itemId.toString());
+
+			User user = userService.findUser(requestDto.getEmail());
+			Item item = itemService.findItem(itemId);
+			checkStock(item);
+			Purchase purchase = Purchase.builder().item(item).user(user).build();
+			purchaseRepository.save(purchase);
+			item.sellOne();
+		} finally {
+			//NamedLock 해제
+			lockItemRepository.releaseLock(itemId.toString());
+		}
 	}
 
 	private void checkStock(Item item) {
