e8bebccc0601a440f6aa526169ea6fc21aa0787e
Jo Woojin <dnwls3525@gmail.com>
2023-09-09 18:26:47 +0900

    refactor : FullText 검색 방식 적용

--- src/main/java/com/tourranger/item/service/ItemServiceImpl.java
+++ src/main/java/com/tourranger/item/service/ItemServiceImpl.java
@@ -1,15 +1,17 @@
 package com.tourranger.item.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
 import com.tourranger.common.error.CustomErrorCode;
 import com.tourranger.common.exception.CustomException;
 import com.tourranger.item.dto.ItemResponseDto;
 import com.tourranger.item.entity.Item;
 import com.tourranger.item.repository.ItemRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -31,14 +33,22 @@ public class ItemServiceImpl implements ItemService {
 
     @Override
     public List<ItemResponseDto> getSearchedItemList(String search, String condition, Pageable pageable) {
+        String keyword;
+        if(search.length()>2){
+            keyword = splitSearchKeywordForNgram(search);
+        }
+        else{
+            keyword = search;
+        }
+        System.out.println(keyword);
 
         return switch (condition) {
             case "latest" -> // 최신순
-                    itemRepository.findByNameContainingOrderByIdDesc(search, pageable).stream().map(ItemResponseDto::new).toList();
+                    itemRepository.searchItemOrderbyIdDesc(keyword, pageable).stream().map(ItemResponseDto::new).toList();
             case "priceLowToHigh" -> // 가격 낮은순
-                    itemRepository.findByNameContainingOrderByDiscountPrice(search, pageable).stream().map(ItemResponseDto::new).toList();
+                    itemRepository.searchItemOrderbyDiscountPrice(keyword, pageable).stream().map(ItemResponseDto::new).toList();
             case "priceHighToLow" -> // 가격 높은순
-                    itemRepository.findByNameContainingOrderByDiscountPriceDesc(search, pageable).stream().map(ItemResponseDto::new).toList();
+                    itemRepository.searchItemOrderbyDiscountPriceDesc(keyword, pageable).stream().map(ItemResponseDto::new).toList();
             default -> null;
         };
     }
@@ -48,5 +58,23 @@ public class ItemServiceImpl implements ItemService {
                 new CustomException(CustomErrorCode.ITEM_NOT_FOUND, null)
         );
     }
+    public String splitSearchKeywordForNgram(String search) {
+        // 공백을 기준으로 문자열 분리
+        String[] tokens = search.split(" ");
 
+        // n-gram 형식으로 변환
+        StringBuilder ngramSearch = new StringBuilder();
+        for (int i = 0; i < tokens.length; i++) {
+            for (int j = 0; j < tokens[i].length(); j++) {
+                if (j + 2 <= tokens[i].length()) {
+                    if (ngramSearch.length() > 0) {
+                        ngramSearch.append(" ");
+                    }
+                    ngramSearch.append("+").append(tokens[i].substring(j, j + 2));
+                }
+            }
+        }
+
+        return ngramSearch.toString();
+    }
 }
