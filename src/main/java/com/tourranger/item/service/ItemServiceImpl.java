0bf4cdfa109c45227a9e4d91a2c40c9a07cbcc1c
Jo Woojin <dnwls3525@gmail.com>
2023-09-13 02:35:38 +0900

    refactor : 추가한 필터 기능에 맞게 동작하도록 getSearchedItemList메서드 수정

--- src/main/java/com/tourranger/item/service/ItemServiceImpl.java
+++ src/main/java/com/tourranger/item/service/ItemServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tourranger.item.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.Pageable;
@@ -17,63 +18,99 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
 
-    private final ItemRepository itemRepository;
+	private final ItemRepository itemRepository;
 
-    @Override
-    public ItemResponseDto getItem(Long itemId) {
-        Item item = findItem(itemId);
-        ItemResponseDto responseDto = new ItemResponseDto(item);
-        return responseDto;
-    }
+	@Override
+	public ItemResponseDto getItem(Long itemId) {
+		Item item = findItem(itemId);
+		ItemResponseDto responseDto = new ItemResponseDto(item);
+		return responseDto;
+	}
 
-    @Override
-    public List<ItemResponseDto> getItemList(Pageable pageable) {
-        return itemRepository.findAllByOrderById(pageable).stream().map(ItemResponseDto::new).toList();
-    }
+	@Override
+	public List<ItemResponseDto> getItemList(Pageable pageable) {
+		return itemRepository.findAllByOrderById(pageable).stream().map(ItemResponseDto::new).toList();
+	}
 
-    @Override
-    public List<ItemResponseDto> getSearchedItemList(String search, String condition, Pageable pageable) {
-        String keyword;
-        if(search.length()>2){
-            keyword = splitSearchKeywordForNgram(search);
-        }
-        else{
-            keyword = search;
-        }
+	@Override
+	public List<ItemResponseDto> getSearchedItemList(
+		String search,
+		String condition,
+		Pageable pageable,
+		String countries,
+		String travelAgencies,
+		String startDate,
+		String endDate,
+		Integer priceValue,
+		boolean priceAbove) {
+		String keyword;
+		if (search.length() > 2) {
+			keyword = splitSearchKeywordForNgram(search);
+		} else {
+			keyword = search;
+		}
 
-        return switch (condition) {
-            case "latest" -> // 최신순
-                    itemRepository.searchItemOrderbyIdDesc(keyword, pageable).stream().map(ItemResponseDto::new).toList();
-            case "priceLowToHigh" -> // 가격 낮은순
-                    itemRepository.searchItemOrderbyDiscountPrice(keyword, pageable).stream().map(ItemResponseDto::new).toList();
-            case "priceHighToLow" -> // 가격 높은순
-                    itemRepository.searchItemOrderbyDiscountPriceDesc(keyword, pageable).stream().map(ItemResponseDto::new).toList();
-            default -> null;
-        };
-    }
+		//국가 정보 List저장
+		List<String> countryList = null;
+		if (!countries.isEmpty()) {
+			countryList = new ArrayList<>();
+			countryList = List.of(countries.split(","));
+		}
+		//여행사 id 타입 변환 String To Integer
+		List<Integer> convertTravelAgencyList = null;
+		if (!travelAgencies.isEmpty()) {
+			String[] travelAgencyList = travelAgencies.split(",");
+			convertTravelAgencyList = new ArrayList<>();
+			for (String num : travelAgencyList) {
+				convertTravelAgencyList.add(Integer.parseInt(num));
+			}
+		}
 
-    public Item findItem(Long id) {
-        return itemRepository.findById(id).orElseThrow(() ->
-                new CustomException(CustomErrorCode.ITEM_NOT_FOUND, null)
-        );
-    }
-    public String splitSearchKeywordForNgram(String search) {
-        // 공백을 기준으로 문자열 분리
-        String[] tokens = search.split(" ");
+		return switch (condition) {
+			case "latest" -> // 최신순
+				itemRepository.searchItemOrderbyIdDesc(keyword, pageable, countryList, convertTravelAgencyList,
+					startDate, endDate, priceValue, priceAbove).stream().map(ItemResponseDto::new).toList();
+			case "priceLowToHigh" -> // 가격 낮은순
+				itemRepository.searchItemOrderbyDiscountPrice(keyword, pageable, countryList, convertTravelAgencyList,
+					startDate, endDate, priceValue, priceAbove).stream().map(ItemResponseDto::new).toList();
+			case "priceHighToLow" -> // 가격 높은순
+				itemRepository.searchItemOrderbyDiscountPriceDesc(keyword, pageable, countryList,
+						convertTravelAgencyList, startDate, endDate, priceValue, priceAbove)
+					.stream()
+					.map(ItemResponseDto::new)
+					.toList();
+			case "noFilterTest" ->//필터 미적용 테스트
+				itemRepository.searchItemNoFilterTest(keyword, pageable).stream().map(ItemResponseDto::new).toList();
+			case "likeTest" -> //LIKE문 조회 테스트
+				itemRepository.searchItemLikeTest(keyword, pageable, countryList, convertTravelAgencyList, startDate,
+					endDate, priceValue, priceAbove).stream().map(ItemResponseDto::new).toList();
+			default -> null;
+		};
+	}
 
-        // n-gram 형식으로 변환
-        StringBuilder ngramSearch = new StringBuilder();
-        for (int i = 0; i < tokens.length; i++) {
-            for (int j = 0; j < tokens[i].length(); j++) {
-                if (j + 2 <= tokens[i].length()) {
-                    if (ngramSearch.length() > 0) {
-                        ngramSearch.append(" ");
-                    }
-                    ngramSearch.append("+").append(tokens[i].substring(j, j + 2));
-                }
-            }
-        }
+	public Item findItem(Long id) {
+		return itemRepository.findById(id).orElseThrow(() ->
+			new CustomException(CustomErrorCode.ITEM_NOT_FOUND, null)
+		);
+	}
 
-        return ngramSearch.toString();
-    }
+	public String splitSearchKeywordForNgram(String search) {
+		// 공백을 기준으로 문자열 분리
+		String[] tokens = search.split(" ");
+
+		// n-gram 형식으로 변환
+		StringBuilder ngramSearch = new StringBuilder();
+		for (int i = 0; i < tokens.length; i++) {
+			for (int j = 0; j < tokens[i].length(); j++) {
+				if (j + 2 <= tokens[i].length()) {
+					if (ngramSearch.length() > 0) {
+						ngramSearch.append(" ");
+					}
+					ngramSearch.append("+").append(tokens[i].substring(j, j + 2));
+				}
+			}
+		}
+
+		return ngramSearch.toString();
+	}
 }
