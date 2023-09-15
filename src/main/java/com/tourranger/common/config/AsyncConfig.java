7c58a8d01a2e8f3f129ef41afd6a194bde8d9bbe
jungeun-choi <jungeun5.choi@gmail.com>
2023-09-15 22:05:11 +0900

    update: 환경변수 삭제

--- src/main/java/com/tourranger/common/config/AsyncConfig.java
+++ src/main/java/com/tourranger/common/config/AsyncConfig.java
@@ -6,22 +6,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import lombok.Value;
-
 @Configuration
 public class AsyncConfig {
-	private int CORE_POOL_SIZE = Integer.parseInt(System.getenv("CORE_POOL_SIZE"));
-	private int MAX_POOL_SIZE = Integer.parseInt(System.getenv("MAX_POOL_SIZE"));
-	private int QUEUE_CAPACITY = Integer.parseInt(System.getenv("QUEUE_CAPACITY"));
-	private String THREAD_NAME_PREFIX = System.getenv("THREAD_NAME_PREFIX");
-
 	@Bean(name = "taskExecutor")
 	public Executor taskExecutor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(CORE_POOL_SIZE); // 스레드 풀의 핵심(core) 스레드 수
-		executor.setMaxPoolSize(MAX_POOL_SIZE); // 스레드 풀의 최대 스레드 수
-		executor.setQueueCapacity(QUEUE_CAPACITY); // 대기 큐의 최대 용량
-		executor.setThreadNamePrefix(THREAD_NAME_PREFIX); // 스레드 이름 접두사
+		executor.setCorePoolSize(4); // 기본 스레드 수
+		executor.setMaxPoolSize(8); // 최대 스레드 수
+		executor.setQueueCapacity(100); // 대기 큐의 최대 사이즈
+		executor.setThreadNamePrefix("Thread Number-"); // 스레드 이름 접두사
 		executor.initialize(); // 스레드 풀을 초기화
 		return executor;
 	}
