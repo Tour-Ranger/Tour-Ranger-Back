37eea7ffae91a1b08a04008b5dbb5567195dcbd2
JisooPyo <audwl326@gmail.com>
2023-09-17 02:27:30 +0900

    init: UserDetailsImpl, UserDetailsServiceImpl, WebSecurityConfig 구현

---
+++ src/main/java/com/tourranger/common/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.tourranger.common.security;
+
+import com.tourranger.user.entity.User;
+import com.tourranger.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepository.findByEmail(email)
+				.orElseThrow(() -> new UsernameNotFoundException("Not Found " + email));
+
+		return new UserDetailsImpl(user);
+	}
+}
