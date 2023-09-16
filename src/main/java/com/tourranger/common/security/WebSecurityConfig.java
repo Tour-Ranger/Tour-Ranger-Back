c5f8625c7f301635a70057af40c989aa730c85fa
JisooPyo <audwl326@gmail.com>
2023-09-17 02:37:38 +0900

    chore: 코드 정렬 및 쓰지 않는 import 제거

--- src/main/java/com/tourranger/common/security/WebSecurityConfig.java
+++ src/main/java/com/tourranger/common/security/WebSecurityConfig.java
@@ -2,9 +2,6 @@ package com.tourranger.common.security;
 
 import com.tourranger.common.error.CustomErrorCode;
 import com.tourranger.common.exception.CustomException;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
--- src/main/java/com/tourranger/user/dto/SignupRequestDto.java
+++ src/main/java/com/tourranger/user/dto/SignupRequestDto.java
@@ -2,7 +2,6 @@ package com.tourranger.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
