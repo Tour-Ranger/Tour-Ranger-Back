2b79bd1e4d9371719dfc5f7b86a24eac62b6523a
JisooPyo <audwl326@gmail.com>
2023-09-17 02:24:28 +0900

    update: USER_ALREADY_EXIST 에러코드 추가

--- src/main/java/com/tourranger/common/error/CustomErrorCode.java
+++ src/main/java/com/tourranger/common/error/CustomErrorCode.java
@@ -13,7 +13,8 @@ public enum CustomErrorCode {
 	IMAGE_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "존재하지 않는 이미지입니다."),
 	TYPE_NOT_IMAGE(HttpStatus.BAD_REQUEST.value(), "파일형식이 이미지파일이 아닙니다."),
 	UPLOAD_NOT_FOUND(HttpStatus.BAD_REQUEST.value(), "파일업로드와 이미지 url 중 한가지 방법으로 업로드 해야합니다."),
-	USER_ALREADY_EXIST(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 사용자입니다.");
+	USER_ALREADY_EXIST(HttpStatus.BAD_REQUEST.value(), "이미 존재하는 사용자입니다."),
+	LOGIN_FAILURE(HttpStatus.UNAUTHORIZED.value(), "로그인 정보가 올바르지 않습니다.");
 	private final int errorCode;
 	private final String errorMessage;
 }
