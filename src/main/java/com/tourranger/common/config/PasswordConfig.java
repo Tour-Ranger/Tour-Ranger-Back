34123846e04c278498c2102c930535274d175ee5
JisooPyo <audwl326@gmail.com>
2023-09-16 23:01:35 +0900

    chore: import 추가

--- src/main/java/com/tourranger/common/config/PasswordConfig.java
+++ src/main/java/com/tourranger/common/config/PasswordConfig.java
@@ -2,6 +2,8 @@ package com.tourranger.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordConfig {
