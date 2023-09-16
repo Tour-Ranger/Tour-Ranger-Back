052d0eefdd977928d6166b33e612acc475b40089
Jungeun Choi <jungeun5.choi@gmail.com>
2023-09-16 18:05:11 +0900

    [feature] 멀티스레드 사용 (#67)
    * init: AsyncConfig
    
    * update: @EnableAsync - TourRangerApplication
    
    * update: @Async - PurchaseServiceImpl
    
    * update: logger.info() 추가
    
    * update: 환경변수 삭제
    
    * chore: 불필요한 log 출력 삭제

--- src/main/java/com/tourranger/TourRangerApplication.java
+++ src/main/java/com/tourranger/TourRangerApplication.java
@@ -2,10 +2,11 @@ package com.tourranger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class TourRangerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TourRangerApplication.class, args);
 	}
